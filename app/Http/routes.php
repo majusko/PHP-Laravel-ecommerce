aa8635aa4cdb7a74b00f6bbd5ae9801c88f9ed6b
Mario Kapusta <mariokapustaa@gmail.com>
2015-08-27 18:10:58 +0200

    vytvor kontakt stranku

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('contact', 'GenericController@contact');
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>Look ... Contact Page Dude!</h1>
+</body>
+</html>
