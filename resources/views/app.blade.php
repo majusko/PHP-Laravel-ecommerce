c43e7137b860533445c190ec259b80ea0f2ac922
Mario Kapusta <mariokapustaa@gmail.com>
2015-09-03 10:26:26 +0200

    theme setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,29 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Creative - Start Bootstrap Theme</title>
+    <title>Fabriano Slovensko</title>
 
     <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="{{URL::asset('css/bootstrap.min.css')}}" type="text/css">
 
     <!-- Custom Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href={{ URL::asset('assets/font-awesome/css/font-awesome.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{URL::asset('font-awesome/css/font-awesome.min.css')}}" type="text/css">
 
     <!-- Plugin CSS -->
-    <link rel="stylesheet" href="../assets/css/animate.min.css" type="text/css">
+    <link rel="stylesheet" href="{{URL::asset('css/animate.min.css')}}" type="text/css">
 
     <!-- Custom CSS -->
-    <link rel="stylesheet" href="../assets/css/creative.css" type="text/css">
+    <link rel="stylesheet" href="{{URL::asset('css/creative.css')}}" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -35,34 +27,30 @@
 </head>
 
 <body id="page-top">
-
 <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
+                <span class="sr-only">Otvor navigáciu</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>
+            <a class="navbar-brand page-scroll" href="#page-top">Fabriano</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
                 <li>
-                    <a class="page-scroll" href="#about">About</a>
+                    <a class="page-scroll" href="#">O Nás</a>
                 </li>
                 <li>
-                    <a class="page-scroll" href="#services">Services</a>
+                    <a class="page-scroll" href="#">Produkty</a>
                 </li>
                 <li>
-                    <a class="page-scroll" href="#portfolio">Portfolio</a>
-                </li>
-                <li>
-                    <a class="page-scroll" href="#contact">Contact</a>
+                    <a class="page-scroll" href="#">Kontakt</a>
                 </li>
             </ul>
         </div>
@@ -73,23 +61,13 @@
 
 @yield('content');
 
-
-<aside class="bg-dark">
-    <div class="container text-center">
-        <div class="call-to-action">
-            <h2>Free Download at Start Bootstrap!</h2>
-            <a href="#" class="btn btn-default btn-xl wow tada">Download Now!</a>
-        </div>
-    </div>
-</aside>
-
 <section id="contact">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 text-center">
-                <h2 class="section-heading">Let's Get In Touch!</h2>
+                <h2 class="section-heading">Naše kotakty</h2>
                 <hr class="primary">
-                <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                <p>Nenašiel si čo potrebuješ? Neváhaj nás kontaktovať</p>
             </div>
             <div class="col-lg-4 col-lg-offset-2 text-center">
                 <i class="fa fa-phone fa-3x wow bounceIn"></i>
@@ -97,25 +75,25 @@
             </div>
             <div class="col-lg-4 text-center">
                 <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
-                <p><a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a></p>
+                <p><a href="mailto:your-email@your-domain.com">davidkapusta@dkramovanie.sk</a></p>
             </div>
         </div>
     </div>
 </section>
 
 <!-- jQuery -->
-<script src="../assets/js/jquery.js"></script>
+<script src="{{URL::asset('js/jquery.js')}}"></script>
 
 <!-- Bootstrap Core JavaScript -->
-<script src="../assets/js/bootstrap.min.js"></script>
+<script src="{{URL::asset('js/bootstrap.min.js')}}"></script>
 
 <!-- Plugin JavaScript -->
-<script src="../assets/js/jquery.easing.min.js"></script>
-<script src="../assets/js/jquery.fittext.js"></script>
-<script src="../assets/js/wow.min.js"></script>
+<script src="{{URL::asset('js/jquery.easing.min.js')}}"></script>
+<script src="{{URL::asset('js/jquery.fittext.js')}}"></script>
+<script src="{{URL::asset('js/wow.min.js')}}"></script>
 
 <!-- Custom Theme JavaScript -->
-<script src="../assets/js/creative.js"></script>
+<script src="{{URL::asset('js/creative.js')}}"></script>
 
 </body>
 
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,26 +6,13 @@
 <header>
     <div class="header-content">
         <div class="header-content-inner">
-            <h1>Your Favorite Source of Free Bootstrap Themes</h1>
+            <h1>Vaše najobľubenejšie umelecké potreby!</h1>
             <hr>
-            <p>Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
-            <a href="#about" class="btn btn-primary btn-xl page-scroll">Find Out More</a>
+            <p>Prdukty od spoločnosti Fabriano sú spojením kvality a moderného dizajnu.</p>
+            <a href="#" class="btn btn-primary btn-xl page-scroll">Naše produkty</a>
         </div>
     </div>
 </header>
-
-<section class="bg-primary" id="about">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 text-center">
-                <h2 class="section-heading">We've got what you need!</h2>
-                <hr class="light">
-                <p class="text-faded">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
-                <a href="#" class="btn btn-default btn-xl">Get Started!</a>
-            </div>
-        </div>
-    </div>
-</section>
 
 <section id="services">
     <div class="container">
